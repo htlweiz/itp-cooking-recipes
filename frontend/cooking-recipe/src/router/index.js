798360db7c7673a507df17d3c9c9206697a2e5eb
ecker200078 <ecker200078@sr.htlweiz.at>
2025-01-13 10:32:44 +0100

    initialize Vue 3 project with Vite, add basic structure and components

---
+++ frontend/cooking-recipe/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+
+const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '/home', component: HomeView },
+  { path: '/login', component: LoginView },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router
