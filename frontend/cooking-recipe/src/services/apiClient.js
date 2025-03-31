12105f96b63a692f03908b19411efae61d909b98
ecker200078 <ecker200078@sr.htlweiz.at>
2025-03-31 11:14:10 +0200

    update API client base URL to use localhost for local development

--- frontend/cooking-recipe/src/services/apiClient.js
+++ frontend/cooking-recipe/src/services/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const apiClient = axios.create({
-  baseURL: 'https://172.31.180.140:8002', 
+  baseURL: 'https://localhost:8002', 
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
